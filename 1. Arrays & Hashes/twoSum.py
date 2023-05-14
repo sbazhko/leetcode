e5e1a68e3217497f313bdb46a771000d4fffc188
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-05-14 11:26:35 +0200

    Arrays&Hashes: Two Sum

---
+++ 1. Arrays & Hashes/twoSum.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/two-sum/
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        leftToSumUp = {}
+        for idx, num in enumerate(nums):
+            if(num in leftToSumUp):
+                return [idx, leftToSumUp[num]]
+            leftToSumUp[target - num] = idx
+        return []
+
+
+solution = Solution()
+print(solution.twoSum([2,7,11,15], 26))
+print(solution.twoSum([3, 2, -1, -2], 0))
