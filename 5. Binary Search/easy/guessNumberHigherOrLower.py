fc7286abee4700d57a82ed81c741eb1d1f79dde8
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-08-05 11:16:31 +0200

    Binary Search: guess Number Higher or Lower

---
+++ 5. Binary Search/easy/guessNumberHigherOrLower.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/guess-number-higher-or-lower/
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+def guess(num: int) -> int:
+    return -1
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, r = 0, n
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+            direction = guess(m)
+            if direction == 0:
+                return m
+            elif direction == 1:
+                l = m + 1
+            else:
+                r = m - 1
