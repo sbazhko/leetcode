caccd42a707c58017e0b015251a8eaea29f4a304
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-08-17 09:17:37 +0200

    Linked List: Find Duplicate Number

---
+++ 6. Linked List/medium/findDuplicateNumber.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/find-the-duplicate-number/description/
+
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while slow != slow2:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+        return slow
