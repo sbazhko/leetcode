39b4fbf0fac482c0fc5b31efc658da963368eefe
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-08-27 13:23:15 +0200

    Trees: Level Order Traversal

---
+++ 7. Trees/medium/levelOrderTraversal.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+from typing import Optional
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = deque()
+        levels = []
+        if root:
+            q.append(root)
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            levels.append(level)
+        return levels
