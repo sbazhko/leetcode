3d54efb82ccb31b01ffde3409ed7fe57248e8a44
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-09-01 23:13:20 +0200

    Bit Manipulation: Single Element

---
+++ Bit Manipulation/easy/singleElement.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res = res ^ n
+        return res
