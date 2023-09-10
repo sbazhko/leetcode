4afe58eab0695fb383e5256bd7e48121e3df389c
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-09-10 11:34:49 +0200

    Linked List: Merge K Sorted Lists

---
+++ 6. Linked List/hard/mergeKSortedLists.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/
+
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        while len(lists) != 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(self.mergeLists(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr = dummy = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 if l1 else l2
+        return dummy.next
+
+
+s = Solution()
+s.mergeKLists(
+    [
+        ListNode(1, ListNode(4, ListNode(5))),
+        ListNode(1, ListNode(3, ListNode(4))),
+        ListNode(2, ListNode(6))
+    ])
