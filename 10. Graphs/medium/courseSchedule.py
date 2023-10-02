37b10218e1a9b04db81e9370b45a5095bb99058d
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-10-02 15:47:14 +0200

    Graphs: Course Schedule

---
+++ 10. Graphs/medium/courseSchedule.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/course-schedule/
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqMap = {i: [] for i in range(numCourses)}
+        for crs, prereq in prerequisites:
+            prereqMap[crs].append(prereq)
+
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if prereqMap[crs] == []:
+                return True
+
+            visited.add(crs)
+            for prereq in prereqMap[crs]:
+                if not dfs(prereq):
+                    return False
+            visited.remove(crs)
+            prereqMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
