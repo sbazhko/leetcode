fb242195ac7d34a7e90d989523a73bc9e1763442
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-10-08 15:58:30 +0200

    Dynamic Programmiing: Climbing Stairs

---
+++ 12. Dynamic Programming/DP_1/easy/climbingStairs.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+
+        for i in range(n - 1):
+            tmp = one
+            one = one + two
+            two = tmp
+        return one
