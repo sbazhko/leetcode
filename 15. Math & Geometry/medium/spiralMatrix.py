40cb6d3074870e2cf93b62e347c3ad2052feb933
siarhei-bazhko <andrew.glbv1997@gmail.com>
2023-10-15 14:18:39 +0200

    Math&Geometry: Spiral Matrix

---
+++ 15. Math & Geometry/medium/spiralMatrix.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/spiral-matrix/
+
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # traverse in rings layer-by-layer shrinking down
+        res = []
+        l, r = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while l < r and top < bottom:
+            for i in range(l, r):
+                res.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom):
+                res.append(matrix[i][r - 1])
+            r -= 1
+            if not (l < r and top < bottom):
+                break
+            for i in range(r - 1, l - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+        return res
+
+
+s = Solution()
+print(s.spiralOrder(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+print(s.spiralOrder(matrix=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))
