414fa01c136cd64a7942405a9f5afb702a7dd4ef
sbazhko <andrew.glbv1997@gmail.com>
2023-10-28 17:08:36 +0200

    Math: Detect Squares

---
+++ 15. Math & Geometry/medium/detectSquares.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/detect-squares/
+
+from collections import defaultdict
+from typing import List
+
+class DetectSquares:
+    def __init__(self):
+        self.ptsCount = defaultdict(int)
+        self.pts = []
+
+    def add(self, point: List[int]) -> None:
+        self.ptsCount[tuple(point)] += 1
+        self.pts.append(point)
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px, py = point
+        for x, y in self.pts:
+            if (abs(py - y) != abs(px - x)) or x == px or y == py:
+                continue
+            res += self.ptsCount[(x, py)] * self.ptsCount[(px, y)]
+        return res
